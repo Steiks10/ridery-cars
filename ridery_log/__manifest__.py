76c8f4e3d9eeeb9224dd20e544d16b7cf48bc5b6
steiks10 <steiker.m2002@gmail.com>
2025-08-04 15:35:53 -0400

    [BASE] Project

---
+++ ridery_log/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Ridery Logs",
+    "version": "1.1",
+    'author': "Steiker Mieles",
+    "license": "OPL-1",
+    "website": "",
+    'contributors': [
+        'Steiker Mieles <steiker.m2002@gmail.com>',
+    ],
+    "category": 'Generic Modules/Ridery',
+    "depends": ['base'],
+    'data': [
+
+    ],
+    'installable': True,
+}
---
+++ ridery_vehicle/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Ridery Vehicle Management",
+    "version": "1.1",
+    'author': "Steiker Mieles",
+    "license": "OPL-1",
+    "website": "",
+    'contributors': [
+        'Steiker Mieles <steiker.m2002@gmail.com>',
+    ],
+    "category": 'Generic Modules/Ridery',
+    "depends": ['base', 'contacts'],
+    'data': [
+
+    ],
+    'installable': True,
+}
