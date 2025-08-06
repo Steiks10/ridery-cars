40e49c69e27a2b1ed1803fbb683c61d26073f4c7
steiks10 <steiker.m2002@gmail.com>
2025-08-06 02:14:27 -0400

    [IMP] Ridery Log with security and implementation in middleware

--- ridery_vehicle/controllers/vehicle.py
+++ ridery_vehicle/controllers/vehicle.py
@@ -14,15 +14,48 @@ class ElearningController(http.Controller):
     @http.route('/ridery/vehicle', auth='public', methods=['POST'], type='http', csrf=False)
     @middleware_ridery_decorator
     def create_vehicle(self, **kwargs):
-        print()
+        http_method = request.httprequest.method
         helper = request.env['response.helper']
+        ridery_logger = request.env['ridery.log'].sudo()
         try:
             data = request.httprequest.get_data()
             request_values = json.loads(data.decode('utf-8'))
-            return helper.success_response({'id': 1})
+
+            stock_location_id = False
+            state_id = request.env['fleet.vehicle.state'].sudo().search([('name', '=', request_values.get('state_id'))])
+            model_id = request.env['fleet.vehicle.model'].sudo().search([('name', '=', request_values.get('model_id'))])
+            driver_id = request.env['res.partner'].sudo().search([('vat', '=', request_values.get('driver_id'))])
+            model_year = request_values.get('model_year')
+            color = request_values.get('color')
+            license_plate = request_values.get('license_plate')
+            if request_values.get('stock_location_id'):
+                stock_location_id = request.env['res.partner'].sudo().search([('name', '=', request_values.get('stock_location_id'))])
+            vehicle_created = self.create_fleet_vehicle(
+                state_id,
+                model_id,
+                driver_id,
+                model_year,
+                color,
+                license_plate,
+                stock_location_id
+            )
+            vehicle_created_response_data = {'id': vehicle_created.id}
+            return helper.success_response(vehicle_created_response_data)
         except Exception as e:
             _logger.error("Invoice creation error: %s", str(e))
-            return helper.error_response(str(e), 500)
+            response_text = str(e)
+            return helper.error_response(response_text, 500)
+
+    def create_fleet_vehicle(self, state_id, model_id, driver_id, model_year, color, license_plate, stock_location_id=False):
+        return request.env['fleet.vehicle'].sudo().create({
+            'state_id': state_id.id,
+            'model_id': model_id.id,
+            'driver_id': driver_id.id,
+            'model_year': model_year,
+            'color': color,
+            'license_plate': license_plate,
+            'stock_location_id': stock_location_id.id,
+        })
 
 
 
